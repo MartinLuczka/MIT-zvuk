c048fba3d59631f67f1ab2a32a61fb82dbb02c3b
Martin Luczka <luc41305@spseol.cz>
2024-06-10 12:21:11 +0200

    úpravy ve škole

--- src/main.c
+++ src/main.c
@@ -3,42 +3,50 @@
 //#include <stdio.h>
 #include "main.h"
 #include "milis.h"
-//#include "delay.h"
+#include "delay.h"
 //#include "uart1.h"
+
+#define tranzistor GPIOG, GPIO_PIN_3
 
 void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
 
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
     GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
+    GPIO_Init(GPIOG, GPIO_PIN_3, GPIO_MODE_OUT_PP_HIGH_SLOW);
 
     init_milis();
-    //init_uart1();
 }
 
 
 int main(void)
 {
-  
     uint32_t time = 0;
-
+    uint32_t time_switch = 0;
+    bool stav_tranzistoru = 0;
     init();
 
-    while (1) {
-#if defined (BTN_PORT) && defined (BTN_PIN)
-        if (milis() - time > 333 && !PUSH(BTN)) {
-#else
-        if (milis() - time > 333 ) {
-#endif
-            REVERSE(LED); 
+    while(1) {
+        if(milis() - time > 5) {
             time = milis();
-            //printf("%ld\n", time);
+            REVERSE(tranzistor);
+            if(milis() - time_switch > 125) {
+                if(stav_tranzistoru == 0) {
+                    LOW(tranzistor);
+                    delay_ms(125);
+                    stav_tranzistoru = 1;
+                }
+                else if(stav_tranzistoru == 1) {
+                    HIGH(tranzistor);
+                    delay_ms(125);
+                    stav_tranzistoru = 0;
+                }
+                time_switch = milis();
+            }
         }
-        //delay_ms(333);
     }
+
 }
 
 /*-------------------------------  Assert -----------------------------------*/
